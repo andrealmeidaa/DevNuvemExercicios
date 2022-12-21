7e150101b688a717699f67615c982ddd38e97077
Andre Almeida <andre.almeida@ifrn.edu.br>
2022-12-21 18:48:40 -0300

    Implementação básica endpoint Django Rest Framework

---
+++ api/python/apieletronicos/apiprodutos/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Produto(models.Model):
+    id=models.AutoField(primary_key=True)
+    nome=models.CharField(max_length=100,blank=False)
+    preco=models.DecimalField(decimal_places=2,max_digits=10)
+    descricao=models.TextField()
+    quantidade_estoque=models.DecimalField(decimal_places=0,max_digits=10)
+
+    class Meta:
+        ordering=['nome']
